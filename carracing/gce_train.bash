73f2e73ba484ba0821593ce345579f686401688d
Anthony Hu <ah2029@cam.ac.uk>
2019-05-22 19:08:37 +0100

    bash command

--- carracing/gce_train.bash
+++ carracing/gce_train.bash
@@ -1,4 +1,4 @@
 export LD_LIBRARY_PATH
 LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu/mesa:/usr/lib/x86_64-linux-gnu/mesa-egl:$LD_LIBRARY_PATH"
 
-xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python3 train.py --name beta5_rtd --num_worker 12 --num_worker_trial 3 --unique_id verif
+xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python3 train.py --name beta5_rtd --num_worker 12 --num_worker_trial 48 --num_episode 1 --unique_id verif
