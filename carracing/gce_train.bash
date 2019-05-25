f7061e622c82f7983e7cf66221f69673fd8008af
Anthony Hu <anth.hu@gmail.com>
2019-05-25 14:50:15 +0100

    Train NSRA on a_concat

--- carracing/gce_train.bash
+++ carracing/gce_train.bash
@@ -1,4 +1,6 @@
 export LD_LIBRARY_PATH
 LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu/mesa:/usr/lib/x86_64-linux-gnu/mesa-egl:$LD_LIBRARY_PATH"
 
-xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python3 train.py --name beta5_rtd --novelty_search --num_worker 12 --num_worker_trial 3 --num_episode 1 --unique_id nsr --novelty_mode a_concat --eval_steps 1 --ns_mode NSR
+xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python3 train.py --name beta5_rtd --novelty_search \
+--num_worker 40 --num_worker_trial 1 --num_episode 1 --eval_steps 1 --novelty_mode a_concat \
+--ns_mode NSRA --unique_id nsra
